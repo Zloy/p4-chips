e1daea113d10aa95336ab84c74910a78796e3a30
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-11-18 11:11:36 +0400

    Implement P4::Chips.[:config, :configure]

--- lib/p4/chips.rb
+++ lib/p4/chips.rb
@@ -1,8 +1,15 @@
 require "p4/chips/version"
 require "p4/chips/player"
+require "hashie"
 
 module P4
   module Chips
-    # Your code goes here...
+    def self.config
+      @config ||= Hashie::Mash.new
+    end
+
+    def self.configure
+      yield self.config
+    end
   end
 end
--- spec/chips_spec.rb
+++ spec/chips_spec.rb
@@ -4,4 +4,25 @@ describe P4::Chips do
   it "should be a module" do
     expect(P4::Chips.class).to eql Module
   end
+
+  let(:config_in_block){
+    P4::Chips.configure do |c|
+      return c
+    end
+  }
+
+  it ".config" do
+    expect(P4::Chips.respond_to? :config).to be true
+    expect{P4::Chips.config.class.should eq(Hashie::Mash)}
+    P4::Chips.config.some_param = "test01"
+    expect{P4::Chips.config.some_param.to eq("test01")}
+  end
+
+  it ".configure" do
+    expect(P4::Chips.respond_to? :configure).to be true
+    expect{config_in_block.class.should eq(Hashie::Mash)}
+
+    config_in_block.some_param = "test02"
+    expect{P4::Chips.config.some_param.to eq("test02")}
+  end
 end
